ef0784c9969990048bbf39b53c4fa5afc6fd9d3a
Алексей Ефимов <lex.efimov@gmail.com>
2018-06-12 21:09:42 +0500

    Initial Commit

---
+++ HW 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW 2
+//
+//  Created by Alexey Efimov on 12.06.2018.
+//  Copyright © 2018 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
